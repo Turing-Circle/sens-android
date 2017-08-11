8f2986102104f28d61f8d5061d9e173277c1f5f6
shubhammalik06 <shubham.malik06@gmail.com>
2017-08-11 14:57:53 +0530

    Merge branch 'master' of F:\Android_Login-And-Register-User-master\sens-android with conflicts.

--- Android_Login-And-Register-User-master/LoginAndRegisterUser/app/src/main/java/com/appnucleus/loginandregisteruser/onoff_settings.java
+++ Android_Login-And-Register-User-master/LoginAndRegisterUser/app/src/main/java/com/appnucleus/loginandregisteruser/onoff_settings.java
@@ -2,26 +2,24 @@ package com.appnucleus.loginandregisteruser;
 
 import android.app.PendingIntent;
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
+import android.support.transition.Transition;
 import android.support.v4.app.Fragment;
 import android.telephony.SmsManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.Toast;
 
 public class onoff_settings extends Fragment {
 
-
+    ImageView imageView;
     Switch aSwitch;
-    String filename = "MySharedString";
-    SharedPreferences someData;
-    public String status = "";
-
-
+    boolean turnedOn = false;
 
     public onoff_settings() {
         // Required empty public constructor
@@ -32,47 +30,46 @@ public class onoff_settings extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_onoff_settings, container, false);
-        someData = this.getActivity().getSharedPreferences(filename,0);
-        final String dataReturned = someData.getString("pumpstatus","couldn't load data");
-        status = dataReturned;
 
+        imageView = (ImageView) view.findViewById(R.id.image_off);
         aSwitch = (Switch) view.findViewById(R.id.switch1);
 
-        if(status.equals("1")){
-            aSwitch.setChecked(true);
-        }
-        else{
-            aSwitch.setChecked(false);
-        }
+
+        aSwitch.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                if(!turnedOn){
+                    imageView.setImageResource(R.drawable.trans_on);
+                    ((TransitionDrawable) imageView.getDrawable()).startTransition(3000);
+                    turnedOn = false;
+                }
+                else{
+                    imageView.setImageResource(R.drawable.trans_off);
+                    ((TransitionDrawable) imageView.getDrawable()).startTransition(3000);
+                    turnedOn = true;
+                }
+            }
+
+        });
 
         aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 
-
                 if (isChecked == true) {
                     send("1");
                     Toast.makeText(getContext(), "ON", Toast.LENGTH_SHORT).show();
-                    SharedPreferences.Editor editor = someData.edit();
-                    editor.putString("pumpstatus", "1");
-                    editor.commit();
                 } else {
                     send("0");
                     Toast.makeText(getContext(), "OFF", Toast.LENGTH_SHORT).show();
-                    SharedPreferences.Editor editor = someData.edit();
-                    editor.putString("pumpstatus", "0");
-                    editor.commit();
                 }
-
             }
         });
-
         return view;
     }
 
 
     public void send(String msgg){
-
 
         String no="9891154325";
 
